4e306ea6049a34e2540d6f1247f28e5cad515a64
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-09-13 23:18:01 +0900

    API の宣言を追加

--- MCI-Test/Form1.vb
+++ MCI-Test/Form1.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form1
 
+    <System.Runtime.InteropServices.DllImport("winmm.dll",
+    CharSet:=System.Runtime.InteropServices.CharSet.Auto)>
+    Private Shared Function mciSendString(ByVal command As String,
+    ByVal buffer As System.Text.StringBuilder,
+    ByVal bufferSize As Integer, ByVal hwndCallback As IntPtr) As Integer
+    End Function
+
+    Private aliasName As String = "MediaFile"
 End Class
